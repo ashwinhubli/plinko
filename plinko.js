07bc8ebfaf64ac255f38b950a293030de52431f3
ashwinhubli <66745007+ashwinhubli@users.noreply.github.com>
2020-07-12 18:03:00 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,17 @@
+class Plinko {
+    constructor(x,y,radius) {
+      var options = {
+          isStatic:true
+      }
+      this.radius = radius;
+      this.body = Bodies.circle(x,y,radius,options);
+      World.add(world, this.body);
+      
+    }
+    display(){
+      var pos =this.body.position;
+      ellipseMode(CENTER);
+      fill("white");
+      ellipse(pos.x, pos.y, this.radius);
+    }
+  }
